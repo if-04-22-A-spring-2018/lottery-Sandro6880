615451ce76720ec7d88d708320d19696ae08697f
sandro6880 <sandro6880@gmail.com>
2018-05-24 20:21:11 +0200

    My commit

--- assignment/lottery.cpp
+++ assignment/lottery.cpp
@@ -10,3 +10,103 @@
  * Implementation of a library for analyzing lottery tips.
  *-----------------------------------------------------------------------------
 */
+/*-----------------------------------------------------------------------------
+ *				HTBLA-Leonding / Class: 2AHIF
+ *-----------------------------------------------------------------------------
+ * Exercise Number: #exercise_number
+ * File:			lottery.c
+ * Author(s):		Ivonne Gattringer
+ * Due Date:
+ *-----------------------------------------------------------------------------
+ * Description:
+ * Implementation of a library for analyzing lottery tips.
+ *-----------------------------------------------------------------------------
+*/
+#include <stdio.h>
+#include "lottery.h"
+
+static char* file_name;
+static char seperator;
+static int* drawed;
+static bool check = false;
+
+bool init_lottery(char* filename, char j){
+    FILE* file= fopen(filename, "r");
+    seperator = j;
+    check = false;
+    file_name = filename;
+    if(file == NULL)return false;
+    fclose(file);
+    return true;
+}
+bool get_tip(int tip, int* array){
+    FILE* file= fopen(file_name, "r");
+    char input[MAX_LINE_LEN] = {0};
+    int number_of_counts = 0;
+    if(tip < 0 || file == 0){
+      fclose(file);
+      return false;
+    }
+    char* output = fgets(input, MAX_LINE_LEN, file);
+    while(output != NULL && number_of_counts < tip){
+        output = fgets(input, MAX_LINE_LEN, file);
+        number_of_counts++;
+        if(output != input){
+            fclose(file);
+            return false;
+        }
+    }
+    if(ftell(file) == EOF){
+        fclose(file);
+        return false;
+    }
+    int pos = 0;
+    while(input[pos] !=seperator && pos < UUID_LEN){
+        pos++;
+    }
+
+    int insertionpos = 0;
+    int tip_number = 0;
+    while(insertionpos < 6){
+        pos++;
+        if(input[pos]<= '9' && input[pos]>= '0'){
+            tip_number = tip_number *10 + (input[pos] - '0');
+        }
+        else{
+            array[insertionpos] = tip_number;
+            tip_number = 0;
+            insertionpos++;
+        }
+    }
+    fclose(file);
+    return true;
+}
+bool set_drawing(int* array){
+    check = true;
+    drawed = array;
+    return false;
+}
+int get_tip_result(int tip){
+    if(!check )return -1;
+    int checktip[6];
+    if(!get_tip(tip, checktip))return -2;
+    int count = 0;
+    for (int i = 0; i < 6; ++i) {
+        for (int j = 0; j < 6; ++j) {
+            if(checktip[i] == drawed[j])count++;
+        }
+    }
+    return count;
+}
+int get_right_tips_count(int tip){
+    if(!check|| tip < 0 || tip > TIP_SIZE)return -1;
+    int tip_pos = 0;
+    int count_right_tips = 0;
+    int check_tip = 0;
+    while(check_tip != -2&& check_tip != -1){
+        check_tip = get_tip_result(tip_pos);
+        if(check_tip == tip)count_right_tips++;
+        tip_pos++;
+    }
+    return count_right_tips;
+}
--- assignment/lottery.h
+++ assignment/lottery.h
@@ -10,7 +10,16 @@
  * Header for lottery tips analysis.
  * ----------------------------------------------------------
  */
-#ifndef ___LOTTERY_H
-#define ___LOTTERY_H
+ #ifndef ___LOTTERY_H
+ #define ___LOTTERY_H
+ #define UUID_LEN   40
+ #define TIP_SIZE   17
+ #define MAX_LINE_LEN   (UUID_LEN + 1 + TIP_SIZE + 1)
 
-#endif
+ bool init_lottery(char* filename, char j);
+ bool get_tip(int tip, int* array);
+ bool set_drawing(int* array);
+ int get_tip_result(int tip);
+ int get_right_tips_count(int tip);
+
+ #endif
