e1ab04ddfe415533841a4bac29ad7e0e5f3aad20
sandro6880 <sandro6880@gmail.com>
2018-05-24 20:22:11 +0200

    My commit

--- assignment/lottery.cpp
+++ assignment/lottery.cpp
@@ -1,27 +1,3 @@
-/*-----------------------------------------------------------------------------
- *				HTBLA-Leonding / Class: <your class name here>
- *-----------------------------------------------------------------------------
- * Exercise Number: #exercise_number#
- * File:			lottery.c
- * Author(s):		Peter Bauer
- * Due Date:		#due#
- *-----------------------------------------------------------------------------
- * Description:
- * Implementation of a library for analyzing lottery tips.
- *-----------------------------------------------------------------------------
-*/
-/*-----------------------------------------------------------------------------
- *				HTBLA-Leonding / Class: 2AHIF
- *-----------------------------------------------------------------------------
- * Exercise Number: #exercise_number
- * File:			lottery.c
- * Author(s):		Ivonne Gattringer
- * Due Date:
- *-----------------------------------------------------------------------------
- * Description:
- * Implementation of a library for analyzing lottery tips.
- *-----------------------------------------------------------------------------
-*/
 #include <stdio.h>
 #include "lottery.h"
 
